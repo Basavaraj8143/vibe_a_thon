df9d15ff3a4b5fc5f03e5d49d540aaa9e397f62e
Basavaraj_m_n <162813862+Basavaraj8143@users.noreply.github.com>
2025-11-27 12:38:01 +0530

    in

---
+++ App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Provider as PaperProvider } from 'react-native-paper';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+import ContactsScreen from './screens/ContactsScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <PaperProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Contacts" component={ContactsScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
+  );
+}
